95bd2b96a390b147e8a507fe62587e7bbaa0ce6d
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2020-06-30 17:08:20 -0400

    Task: Solution 3

--- src/answers.py
+++ src/answers.py
@@ -1,3 +1,4 @@
+#Solution 1
 nums = [1, 2, 3, 4, 5]
 
 def identity(nums):
@@ -5,11 +6,18 @@ def identity(nums):
 
 identity(nums)
 
+#Solution 2
 nums = [1, 2, 4, 6, 8, 33, 78]
-
 
 def doubled(nums):
     print([x * 2 for x in nums])
 
-
 doubled(nums)
+
+#Solution 3
+nums = [1, 2, 4, 6, 8, 100, 78]
+
+def squared(nums):
+    print([x ** 2 for x in nums])
+
+squared(nums)
