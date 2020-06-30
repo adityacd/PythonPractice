4f9c424bd92c6a804db0c94c9c8387cc7aceed55
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2020-06-30 17:09:34 -0400

    Task: Solution 4

--- src/answers.py
+++ src/answers.py
@@ -21,3 +21,11 @@ def squared(nums):
     print([x ** 2 for x in nums])
 
 squared(nums)
+
+#Solution 4
+nums = [1, 2, 4, 6, 8, 100, 78]
+
+def evens(nums):
+    print([x for x in nums if x % 2 == 0])
+
+evens(nums)
